8228c8484986e8dcdfe812d6a2d3b9441de6623a
Ryan R <rwrutled@uncc.edu>
2012-07-03 07:22:31 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VikiSample::Application.config.secret_token = 'b6cc914a0df307dded18a158358ed6ec78636d737d33db0d2e5608b31bc7395880de55fa83c874f25a463144afeb904d5dda81f21eb356757dd1f0a622078e2f'
